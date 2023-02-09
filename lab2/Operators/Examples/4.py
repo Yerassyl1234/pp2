6bb88f5a74976ef7b10f9ffdd51430cc0a9808a7
Yerassyl Orazkhan <y_orazkhan@kbtu.kz>
2023-02-09 10:09:05 +0600

    lab 1 and 2

---
+++ lab2/Operators/Examples/4.py
@@ -0,0 +1,9 @@
+"""
+Operator	Name	                    Example	
+==	        Equal	                    x == y	
+!=	        Not equal	                x != y	
+>	        Greater than	            x > y	
+<	        Less than	                x < y	
+>=	        Greater than or equal to	x >= y	
+<=	        Less than or equal to	    x <= y 
+"""
