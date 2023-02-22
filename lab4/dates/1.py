b077e8ae84ce40eb4260a9a67bcb07ef7a8f8579
Yerassyl Orazkhan <y_orazkhan@kbtu.kz>
2023-02-22 21:21:02 +0600

    1

--- lab4/dates/1.py
+++
@@ -1,4 +0,0 @@
-from datetime import date
-from datetime import timedelta
-print(date.today())
-print(date.today()-timedelta(5))
--- lab4/dates/2.py
+++
@@ -1,5 +0,0 @@
-from datetime import date
-from datetime import timedelta
-print(date.today()-timedelta(1))
-print(date.today())
-print(date.today()+timedelta(1))
